ce75e0cfc9747df4b22987e03a51afb917d7fc01
Serhii Petrash <morismatie@gmail.com>
2020-10-28 10:23:59 +0200

    Lesson 9 Add Order page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -2,8 +2,60 @@
    <v-container>
        <v-row>
            <v-col>
-               <h1>Orders page</h1>
+               <v-card
+               class="mx-auto"
+               max-width="500"
+               >
+                   <v-list>
+                    <v-list-item
+                    v-for="order in orders"
+                    :key="order.id"
+                    >
+                    
+                        <v-list-item-action>
+                        <v-checkbox 
+                        color="success"
+                        @change="markDone(order)"
+                        :input-value="order.done"></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                        <v-list-item-title>{{ order.name }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                            <v-btn
+                            :to="'/ad/' + order.adId"
+                            class="primary"
+                            >Open</v-btn>
+                        </v-list-item-action>
+                    </v-list-item>
+                   </v-list>
+               </v-card>
            </v-col>
        </v-row>
     </v-container> 
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            orders: [
+                {
+                    id: 'id1',
+                    name: 'Vladilen',
+                    phone: '380-96-9630001',
+                    adId: '111',
+                    done: false
+                }
+                ]
+        }
+    },
+    methods: {
+        markDone(order) {
+            order.done = true
+        }
+    }
+}
+</script>>
